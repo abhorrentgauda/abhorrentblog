497982aa62a6bf6c4715f49152c52f176a586d4a
abhorrentgauda <trainxcrypt0x@gmail.com>
2023-01-15 14:18:27 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<div>Hi</div>);
+
+console.log('hello');
